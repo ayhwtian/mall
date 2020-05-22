94ffc09b1375ac06eb48e602b5b7d81c60910d52
Honwei <ayhwtian@163.com>
2020-05-22 18:34:09 +0800

    '20200522'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="home">
-    <nav-bar>
+    <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
   </div>
@@ -8,14 +8,34 @@
 
 <script>
   import NavBar from "components/common/navbar/NavBar"
+  import {getHomeMutidata} from "@/network/home";
 
   export default {
     name: 'Home',
     components: {
       NavBar
+    },
+    data() {
+      return {
+        banner: [],
+        recommend:[]
+      }
+    },
+    created() {
+      //1.请求多个数据
+      getHomeMutidata()
+        .then( res => {
+          console.log(res);
+          this.banner = res.data.banner.list;
+          this.recommend = res.data.recommend.list
+        })
     }
   }
 </script>
 
 <style>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #ffffff;
+  }
 </style>
