4ce43b771d73b3616652f0ac06674409a5ffbdf5
Honwei <ayhwtian@163.com>
2020-05-23 17:12:03 +0800

    '20200523'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,25 +3,29 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recomment-view :recommends="recommends"></recomment-view>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar"
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
+
   import {getHomeMutidata} from "@/network/home";
-  import {Swiper,SwiperItem} from "components/common/swiper"
 
   export default {
     name: 'Home',
     components: {
       NavBar,
-      Swiper,
-      SwiperItem
+      HomeSwiper,
+      RecommendView
     },
     data() {
       return {
-        banner: [],
-        recommend:[]
+        banners: [],
+        recommends:[]
       }
     },
     created() {
@@ -29,8 +33,8 @@
       getHomeMutidata()
         .then( res => {
           console.log(res);
-          this.banner = res.data.banner.list;
-          this.recommend = res.data.recommend.list
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list
         })
     }
   }
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "RecommendView",
+      props: {
+        recommends: {
+          type: Array,
+          default() {
+            return [];
+          }
+        }
+        }
+      }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+
+    padding: 15px 0 30px;
+    border-bottom: 6px silver;
+  }
+  .recommend-item {
+    flex: 1;
+  }
+  .recommend-item img {
+    width: 65px;
+    height: 65px;
+    margin-bottom: 10px;
+  }
+</style>
